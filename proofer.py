f0edee07e578b7d6923b0fc0ed6e09d652322b20
Casey Schneider-Mizell <caseysm@gmail.com>
2020-07-07 23:51:54 -0700

    better branch selection

--- proofer.py
+++ proofer.py
@@ -76,6 +76,7 @@ def _parse_state(state, seg_layer='select_to_proof', ignore_layer='ignore_beyond
 
 def proofreading_statebuilder(client,
                               fixed_ids,
+                              grouped=False,
                               bp_tags=BP_TAGS,
                               bp_color=BP_COLOR,
                               ep_tags=EP_TAGS,
@@ -85,9 +86,15 @@ def proofreading_statebuilder(client,
         source=client.info.image_source(), contrast_controls=True)
     seg = nglui.statebuilder.SegmentationLayerConfig(
         source=client.info.segmentation_source(), fixed_ids=fixed_ids)
-    bp_points = nglui.statebuilder.PointMapper(set_position=True)
+
+    if grouped:
+        bp_points = nglui.statebuilder.PointMapper(
+            point_column='pt', group_column='group', set_position=True)
+    else:
+        bp_points = nglui.statebuilder.PointMapper(
+            point_column='pt', set_position=True)
     bp_annos = nglui.statebuilder.AnnotationLayerConfig(
-        name='branch_points', color=bp_color, mapping_rules=bp_points, array_data=True, tags=bp_tags, active=True)
+        name='branch_points', color=bp_color, mapping_rules=bp_points, tags=bp_tags, active=True)
     sb_bp = nglui.statebuilder.StateBuilder([img, seg, bp_annos])
 
     ep_points = nglui.statebuilder.PointMapper()
@@ -95,24 +102,64 @@ def proofreading_statebuilder(client,
         name='end_points', color=ep_color, mapping_rules=ep_points, array_data=True, tags=ep_tags, active=False)
     sb_ep = nglui.statebuilder.StateBuilder([ep_annos])
 
-    stop_points = nglui.statebuilder.PointMapper()
+    stop_points = nglui.statebuilder.SphereMapper()
     stop_annos = nglui.statebuilder.AnnotationLayerConfig(
-        name='axon_point', color=stop_color, mapping_rules=ep_points, array_data=True, tags=ep_tags, active=False)
+        name='axon_point', color=stop_color, mapping_rules=stop_points, array_data=True, active=False)
     sb_stop = nglui.statebuilder.StateBuilder([stop_annos])
     return nglui.statebuilder.ChainedStateBuilder([sb_bp, sb_ep, sb_stop])
 
 
-def _branch_points_ordered(nrn):
-    bps = nrn.branch_points
-    bps = bps[bps != nrn.root]
-    bp_ord = np.argsort(nrn.distance_to_root(bps))
-    return nrn.mesh.vertices[bps[bp_ord]]
+def _branch_and_end_points_ordered(nrn):
+    cps = nrn.skeleton.cover_paths
+    cp_ends = np.array([nrn.skeleton.parent_nodes(cp[-1]) for cp in cps])
 
+    bps = nrn.skeleton.branch_points
+    cp_queue = [0]
+    primary_cps = [ii for ii, cp in enumerate(cp_ends) if cp == nrn.skeleton.root]
+    cp_queue.extend(primary_cps)
 
-def _end_points_ordered(nrn):
-    eps = nrn.end_points
-    ep_ord = np.argsort(nrn.distance_to_root(eps))
-    return nrn.mesh.vertices[eps[ep_ord]]
+    is_done = np.full(len(cp_ends), False)
+    bp_groups = []
+    eps = []
+    while not np.all(is_done):
+        ind_to_do = cp_queue.pop(0)
+        is_done[ind_to_do] = True
+        cp = cps[ind_to_do]
+
+        # Get branch points
+        bps_on_cp = bps[np.isin(bps, cp)]
+        bp_groups.append(bps_on_cp[bps_on_cp != nrn.skeleton.root])
+
+        # Get end point
+        eps.append(cp[0])
+
+        # Get other cps to check
+        cp_offshoots = np.flatnonzero(np.isin(cp_ends, cp))
+        if len(cp_offshoots) > 0:
+            cp_offshoots = cp_offshoots[~is_done[cp_offshoots]]
+            lens = np.array([nrn.skeleton.distance_to_root[cps[ii][0]] -
+                             nrn.skeleton.distance_to_root[cps[ii][-1]] for ii in cp_offshoots])
+            cp_offshoots = cp_offshoots[np.argsort(lens)]
+            for ind in cp_offshoots:
+                if ind not in cp_queue:
+                    cp_queue.insert(0, ind)
+
+    bp_points = []
+    for skinds in bp_groups:
+        if len(skinds) > 0:
+            bp_points.append(nrn.skeleton.vertices[skinds])
+
+    ep_points = []
+    for skind in eps:
+        ep_points.append(nrn.skeleton.vertices[skind])
+    ep_points = np.vstack(ep_points)
+
+    return bp_points, ep_points
+
+# def _end_points_ordered(nrn):
+#     eps = nrn.end_points
+#     ep_ord = np.argsort(nrn.distance_to_root(eps))
+#     return nrn.mesh.vertices[eps[ep_ord]]
 
 
 def get_topo_points(client, segs, annos, min_size=1000, voxel_resolution=np.array([4, 4, 40])):
@@ -126,7 +173,7 @@ def get_topo_points(client, segs, annos, min_size=1000, voxel_resolution=np.arra
     for ii, oid in enumerate(segs):
         mesh = mm.mesh(seg_id=oid)
 
-        lcs = mesh_filters.filter_components_by_size(mesh, min_size=min_size)
+        lcs = mesh_filters.filter_largest_component(mesh)
         meshf = mesh.apply_mask(lcs)
 
         nrn = meshwork.Meshwork(meshf)
@@ -146,13 +193,34 @@ def get_topo_points(client, segs, annos, min_size=1000, voxel_resolution=np.arra
             for mask in ignore_masks:
                 nrn.apply_mask(mask)
 
-        bps = _branch_points_ordered(nrn) / voxel_resolution
-        all_bps.append(bps)
+        bps, eps = _branch_and_end_points_ordered(nrn)
+        all_bps.append([bp/voxel_resolution for bp in bps])
+        all_eps.append(eps/voxel_resolution)
+        # bps = _branch_points_ordered(nrn) / voxel_resolution
+        # all_bps.append(bps)
 
-        eps = _end_points_ordered(nrn) / voxel_resolution
-        all_eps.append(eps)
+        # eps = _end_points_ordered(nrn) / voxel_resolution
+        # all_eps.append(eps)
 
-    return [np.vstack(all_bps), np.vstack(all_eps), np.vstack(annos['ignore'])]
+    return [all_bps, np.vstack(all_eps), np.vstack(annos['ignore'])]
+
+
+def _format_data(data, sphere_radius=1000):
+    all_bps, all_eps, ignore_points = data
+    bp_groups = []
+    ii = 0
+    all_bps_flat = []
+    for bpg in all_bps:
+        for bps in bpg:
+            all_bps_flat.append(bps)
+            bp_groups.append(np.full(len(bps), ii))
+            ii += 1
+
+    all_bps = np.vstack(all_bps_flat)
+    bp_groups = np.concatenate(bp_groups)
+    bp_df = pd.DataFrame(data={'pt': all_bps.tolist(), 'group': bp_groups})
+    ignore_points = (ignore_points, np.full(len(ignore_points), sphere_radius))
+    return bp_df, all_eps, ignore_points
 
 
 def build_proofreading_state(state_url, datastack, render_as='url'):
@@ -161,6 +229,7 @@ def build_proofreading_state(state_url, datastack, render_as='url'):
     segs, annos = parse_state(state_url, datastack, client)
     sb = proofreading_statebuilder(client, segs)
     data = get_topo_points(client, segs, annos, min_size=1000)
+    data = _format_data(data)
     return sb.render_state(data,
                            return_as=render_as,
                            url_prefix=client.info.info_cache.get(datastack).get('viewer_site'))
